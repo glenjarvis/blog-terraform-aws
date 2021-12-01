47ea44d3d8367b5903ea57dd7513589a3ced9e0a
Glen Jarvis <glen@glenjarvis.com>
2021-11-30 21:56:55 -0800

    Switch the provider to S3 backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "com.glenjarvis.demo.terraform"
+    key    = "state/terraform_state"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
